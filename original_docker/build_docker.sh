a5fa5b00728e4f7bb290cd984d72cc4164ef7ff5
Shantanu Singh <shsingh@broadinstitute.org>
2024-05-31 20:29:57 +0200

    Delete original_docker directory

--- original_docker/build_docker.sh
+++
@@ -1,9 +0,0 @@
-# Copyright 2022 Verily Life Sciences LLC
-# Use of this source code is governed by a BSD-style license that can be found in the LICENSE file or at
-# https://developers.google.com/open-source/licenses/bsd
-
-#!/bin/bash
-
-gcloud --project terra-solutions-jump-cp-dev builds submit \
-  --timeout 20m \
-  --tag gcr.io/terra-solutions-jump-cp-dev/embedding_creation:`date +"%Y%m%d_%H%M%S"` .
